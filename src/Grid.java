40dec4bcfed5e5139148826345e519d0d9f11e74
Ethan <elet1@students.waikato.ac.nz>
2024-05-25 16:40:18 +1200

    Grid methods

--- src/Grid.java
+++ src/Grid.java
@@ -1,4 +1,8 @@
 package src;
 
-public class Grid {
+import java.util.ArrayList;
+
+public class Grid
+{
+    ArrayList<Unit> unitsPresent;
 }
--- src/container.java
+++ src/container.java
@@ -4,4 +4,5 @@ public abstract class container extends item
 {
     int maxWeight;
     int maxVolume;
+
 }
